40174f96d00e103e9611509e72112844d1945f33
HP <HP@DESKTOP-2DC4B1N>
2022-07-19 15:32:52 +0530

    insta change

---
+++ udaan/src/test/java/testing/OnlineTest.java
@@ -0,0 +1,15 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class OnlineTest {
+	@Test
+	public void ZomatoTest() {
+		System.out.println("Online food delivery");
+	}
+	@Test
+	public void SwiggyTest() {
+		System.out.println("Swiggy food is good");
+	}
+
+}
