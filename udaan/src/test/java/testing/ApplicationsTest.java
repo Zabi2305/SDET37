e62a97286ac0c72ee3dc257c7edf46edfffc1e14
HP <HP@DESKTOP-2DC4B1N>
2022-07-19 12:17:20 +0530

    1st commit

---
+++ udaan/src/test/java/testing/ApplicationsTest.java
@@ -0,0 +1,14 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class ApplicationsTest {
+	@Test
+	public void watsTest() {
+		System.out.println("wats up is the one of the most used ap for communication ");
+	}
+	@Test
+	public void instagramTest() {
+		System.out.println("Instagram is the one of the good social network");
+	}
+}
