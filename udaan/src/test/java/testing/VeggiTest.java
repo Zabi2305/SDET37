4436435879cc3824f25efb821aa5818ccf79c2c3
HP <HP@DESKTOP-2DC4B1N>
2022-07-20 16:42:08 +0530

    mm

--- udaan/src/test/java/testing/VeggiTest.java
+++ udaan/src/test/java/testing/VeggiTest.java
@@ -12,6 +12,15 @@ public class VeggiTest {
 		System.out.println("Sehat ke leya acha");
 		System.out.println("Green veggies are good");
 		System.out.println("Green veggies are haribari");
+		System.out.println("123");
+	}
+	
+	public void Carrot() {
+		String BROWSER = System.getProperty("browser");
+		System.out.println("Browser +------>" +BROWSER);
+		
+		String URL = System.getProperty("url");
+		System.out.println("Url +--------->" + URL);
 	}
 
 }
